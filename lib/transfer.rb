2c83ce708fdf3b469762565fbde8b03fec2f5769
Ben Yellin <byellindc@gmail.com>
2019-01-10 14:46:40 -0500

    initial commit

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,49 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    if !@sender.valid? || !@receiver.valid?
+      return false
+    elsif !@sender.has_amount(@amount)
+      return false
+    else
+      return true
+    end
+  end
+
+  def pending?
+    @status == 'pending'
+  end
+
+  def completed?
+    @status == 'complete'
+  end
+
+
+  def execute_transaction
+    # binding.pry
+    if !self.valid?
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    elsif self.pending?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if self.completed? && @receiver.has_amount(@amount)
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
+  end
 end
